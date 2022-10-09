c286664c9619079876a7a38ba30b45a03f7ded81
유기훈 <98267772+62hoon99@users.noreply.github.com>
2022-10-09 14:38:42 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+        int tmp = x;
+        int num = 0;
+        
+        while(tmp > 0){
+            num += tmp % 10;
+            tmp /= 10;
+        }
+        
+        return (x % num == 0);
+    }
+}
