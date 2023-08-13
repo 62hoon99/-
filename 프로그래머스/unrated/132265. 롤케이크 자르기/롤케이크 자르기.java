8b95006d10501d7e36f9918c21fee2f5924e3c0c
유기훈 <98267772+62hoon99@users.noreply.github.com>
2023-08-13 22:30:08 +0900

    [unrated] Title: 롤케이크 자르기, Time: 289.99 ms, Memory: 191 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] toppings) {
+        int answer = 0;
+        
+        Person old = new Person();
+        Person yg = new Person();
+        
+        for (int topping : toppings) {
+            yg.addDesert(topping);
+        }
+        
+        for (int topping : toppings) {
+            old.addDesert(topping);
+            yg.removeDesert(topping);
+            if (old.count == yg.count) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    static class Person {
+        private Map<Integer, Integer> dessertMap;
+        private int count;
+        
+        public Person() {
+            dessertMap = new HashMap<>();
+            count = 0;
+        }
+        
+        public void addDesert(int dessert) {
+            dessertMap.put(dessert, dessertMap.getOrDefault(dessert, 0) + 1);
+            if (dessertMap.get(dessert) == 1) {
+                count++;
+            }
+        }
+        
+        public void removeDesert(int dessert) {
+            dessertMap.put(dessert, dessertMap.getOrDefault(dessert, 0) - 1);
+            if (dessertMap.get(dessert) == 0) {
+                count--;
+            }
+        }
+        
+        public int getCount() {
+            return count;
+        }
+    }
+}
