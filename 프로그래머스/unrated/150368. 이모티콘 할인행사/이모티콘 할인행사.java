b68f147e213dae14a278a38e041f70f54b04d764
유기훈 <98267772+62hoon99@users.noreply.github.com>
2023-10-07 21:01:16 +0900

    [unrated] Title: 이모티콘 할인행사, Time: 5.87 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,85 +1,74 @@
-
-
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class Solution {
-
-    private static final int[] discountArray = {10, 20, 30 ,40};
+    private static final int[] discountArray = {10, 20, 30, 40};
 
     public int[] solution(int[][] usersArray, int[] emoticons) {
 
-        for (int[] userArray : usersArray) {
-            users.add(new User(userArray[0], userArray[1]));
-        }
+        List<User> users = Arrays.stream(usersArray).map(u -> new User(u[0], u[1])).collect(Collectors.toList());
 
-        find(emoticons, 0);
-
-        int[] answer = new int[2];
-        answer[0] = countAnswer;
-        answer[1] = moneyAnswer;
-        return answer;
+        return find(emoticons, users, 0);
     }
 
-    private int countAnswer = 0;
-    private int moneyAnswer = 0;
-    private final List<User> users = new ArrayList<>();
-
-    private void find(int[] emoticons, int depth) {
+    private int[] find(int[] emoticons, List<User> users, int depth) {
         if (emoticons.length == depth) {
-            int lastCount = 0;
-            int lastMoney = 0;
+            int lastCount = (int) users.stream().filter(User::isEmoticonPlusServiceAvailable).count();
+            int lastMoney = users.stream().filter(user -> !user.isEmoticonPlusServiceAvailable())
+                    .mapToInt(User::getPurchaseAmount).sum();
 
-            for (User user : users) {
-                if (user.isBuyService()) {
-                    lastCount++;
-                } else {
-                    lastMoney += user.buyMoney;
-                }
-            }
-
-            if (countAnswer == lastCount && moneyAnswer < lastMoney) {
-                moneyAnswer = lastMoney;
-            } else if (countAnswer < lastCount) {
-                countAnswer = lastCount;
-                moneyAnswer = lastMoney;
-            }
-            return;
+            return new int[]{lastCount, lastMoney};
         }
+
+        int[] maxResult = new int[2];
 
         for (int discount : discountArray) {
             users.forEach(user -> user.buy(emoticons[depth], discount));
-            find(emoticons, depth + 1);
+
+            int[] result = find(emoticons, users, depth + 1);
+            if ((maxResult[0] == result[0] && maxResult[1] < result[1]) || (maxResult[0] < result[0])) {
+                maxResult = result;
+            }
+
             users.forEach(user -> user.sell(emoticons[depth], discount));
         }
+
+        return maxResult;
     }
 
-    private class User {
-        private int discount;
-        private int moneyLine;
+    private static class User {
+        private final int ratio;
+        private final int limit;
+        private int purchaseAmount;
 
-        private int buyMoney = 0;
-
-        public User(int discount, int moneyLine) {
-            this.discount = discount;
-            this.moneyLine = moneyLine;
+        public User(int ratio, int limit) {
+            this.ratio = ratio;
+            this.limit = limit;
         }
 
-        public boolean isBuyService() {
-            return buyMoney >= moneyLine;
+        public int getPurchaseAmount() {
+            return purchaseAmount;
         }
 
-        public void buy(int money, int discount) {
-            if (this.discount <= discount) {
-                buyMoney += money * (100 - discount) / 100;
+        public boolean isEmoticonPlusServiceAvailable() {
+            return purchaseAmount >= limit;
+        }
+
+        public void buy(int amount, int ratio) {
+            if (this.ratio <= ratio) {
+                purchaseAmount += calculateDiscountedAmount(amount, ratio);
             }
         }
 
-        public void sell(int money, int discount) {
-            if (this.discount <= discount) {
-                buyMoney -= money * (100 - discount) / 100;
+        public void sell(int amount, int ratio) {
+            if (this.ratio <= ratio) {
+                purchaseAmount -= calculateDiscountedAmount(amount, ratio);
             }
         }
 
+        private int calculateDiscountedAmount(int amount, int ratio) {
+            return amount * (100 - ratio) / 100;
+        }
     }
 }
